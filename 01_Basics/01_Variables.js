4a0da9bcab2384421598d2bf43552da2941d453e
Kevin <kevingeorge422@gmail.com>
2025-07-24 20:01:11 +0530

    variables completed

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,12 @@
+// working with variables: var, let and const
+
+var name = "Kevin George";
+console.log(name);
+
+var number;
+number = 10;
+console.log(number);
+
+const pi = 3.14;
+console.log(pi);
+
