7579ac93b6af426190628fd07f9958ec658bdb59
Kevin <kevingeorge422@gmail.com>
2025-07-24 20:19:32 +0530

    simple change

--- 01_Basics/01_Variables.js
+++ 01_Basics/01_Variables.js
@@ -10,3 +10,12 @@ console.log(number);
 const pi = 3.14;
 console.log(pi);
 
+const fullName = "Kevin George";
+var email = "test@gmail.com";
+var password = "12345";
+
+console.log(`
+        User is : ${fullName}
+        Email is : ${email}
+        Password is : ${password}
+    `)
